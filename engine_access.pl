8ce6736b3b0d1e809e84665498a1087225afb090
Ray Richardson <exfenestram@yahoo.com>
2015-08-04 11:52:02 -0700

    Initial Split version

---
+++ engine_access.pl
@@ -0,0 +1,215 @@
+% engine_access.pl - Access to the HPCE from Prolog
+% Copyright 2015, Simularity, Inc
+
+:- module(engine_access, [get_segments/2, get_item_size/3, engine_get_type/3,
+			  engine_get_object/4, engine_get_action/3, engine_fold/8,
+			  engine_types/2, engine_save/1, engine_save/2,
+			  engine_restore/1, engine_restore/2]).
+
+:- use_module(library(http/http_header)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/json)).
+
+
+:- use_module(library(http/http_header)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/json)).
+
+
+get_segments(Host:Port, Segments) :-
+	format(atom(URL), 'http://~w:~w/segments', [Host, Port]),
+	% This one doesn't have keep alive
+	http_get(URL, JSON, []),
+	atom_json_term(JSON, JTerm, []),
+	JTerm = json(JSONList),
+	member(segments=JList, JSONList),
+	maplist(to_segment, JList, Segments), !.
+
+get_segments(Host:Port, _) :-
+	throw(error(engine_access(Host:Port), get_segments/2)).
+
+
+
+
+get_item_size(Host:Port, Max, Min) :-
+	format(atom(URL), 'http://~w:~w/itemsize', [Host, Port]),
+	% This one doesn't have keep alive
+	http_get(URL, JSON, []),
+	atom_json_term(JSON, JTerm, []),
+	JTerm = json(JSONList),
+	member(max=Max, JSONList),
+	member(min=Min, JSONList),
+	!.
+
+to_segment(json(List), segment(Number, Host:Port)) :-
+	member(segment=Number, List),
+	member(host=Host, List),
+	member(sport=Port, List).
+
+engine_get_type(_Host:_Port, Name, Type) :-
+	Name =.. [Class, Value],
+	Type =.. [Class, Hash],
+	integer(Value),
+	Hash = Value, !.
+
+engine_get_type(Host:Port, Name, Type) :-
+	Name =.. [Class, Value],
+	Type =.. [Class, Hash],
+
+	format(atom(URL), 'http://~w:~w/convert_type?class=~w&name=~w',
+	       [Host, Port, Class, Value]),
+	http_get(URL, JDoc, []),
+	((atom_json_term(JDoc, JSON, []),
+	  JSON=json(JList), member(status=0, JList),
+	  member(id=Hash, JList)) ->
+	 assertz(type_map(Name, Type))
+	;
+	 throw(error(web_transaction(Host:Port, URL), get_type/3))).
+
+engine_get_object(_, ID, _Type, ID) :-
+	integer(ID), !.
+
+engine_get_object(Host:Port, ObjName, Type, Code) :-
+	engine_get_type(Host:Port, Type, TypeSpec),
+	TypeSpec =.. [Class, TypeId],
+
+	uri_encoded(query_value, ObjName, Encoded), 
+	format(atom(URL), 'http://~w:~w/convert_object?name=~w&class=~w&type=~w', [Host, Port, Encoded, Class, TypeId]),
+
+	http_get(URL, JDoc, []),
+
+	((atom_json_term(JDoc, Json, []),
+	  Json=json(JList), member(status=0, JList),
+	  member(id=Code, JList)) ->
+
+	 asserta(object_def(ObjName, TypeSpec, Code))
+	;
+	 throw(error(web_transaction(Host:Port, URL), get_object/4))).
+
+engine_get_action(_, Action, Action) :-
+	integer(Action), !.
+engine_get_action(Host:Port, Action, Code) :-
+	format(atom(URL), 'http://~w:~w/convert_action?name=~w',
+	       [Host, Port, Action]),
+
+	http_get(URL, JDoc, []),
+	 
+	((atom_json_term(JDoc, JSon, []), JSon = json(JList),
+	  member(status=0, JList), member(id=Code, JList)) ->
+	 asserta(action_def(Action, Code))
+	;
+	 throw(error(web_transaction(Host:Port, URL), get_action/3))).
+
+
+
+map_action(forward(Val), faction=Val).
+map_action(reverse(Val), raction=Val).
+map_action(Val, action=Val) :- atomic(Val).
+
+map_type(subject(S), stype=S).
+map_type(object(O), otype=O).
+
+corr_to_term(json(JList), correlation(Object, Value, I, A2)) :-
+	member(type=Type, JList),
+	member(item=Item, JList),
+	member(value=Value, JList),
+	member(i=I, JList),
+	member(a2=A2, JList),
+	(member(class=Class, JList) ->
+	 true
+	;
+	 Class = object),
+	(member(obj_name=Name, JList) ->
+	 ObjValue = Item-Name
+	;
+	 ObjValue = Item),
+	Object =.. [Class, Type, ObjValue], !.
+
+engine_fold(Host:Port, Expr, Actions, Types, Metric, Legit, Count, Res) :-
+	write_term_to_codes(Expr, Codes, []),
+	maplist(map_action, Actions, URL_Act),
+	maplist(map_type, Types, URL_Type),
+
+	flatten([URL_Act, URL_Type, [metric=Metric, legit=Legit,
+				     count=Count, use_legit=true]],
+		URL_Terms),
+	maplist(term_to_atom, URL_Terms, URL_Atoms),
+	atomic_list_concat(URL_Atoms, '&', URL_String),
+	format(atom(URL), 'http://~w:~w/expression?~w', [Host, Port,
+							 URL_String]),
+	http_post(URL, codes(Codes), Result, []),
+	atom_json_term(Result, JTerm, []),
+	JTerm = json(JSONList),
+	member(status=Status, JSONList),
+	(Status =:= 0 ->
+	 member(a1=A1, JSONList),
+	 member(global=Global, JSONList),
+	 member(correlation=Corr, JSONList),
+	 maplist(corr_to_term, Corr, List),!,
+	 Res = results(A1, Global, List)
+	;
+	 throw(status(Status))).
+
+engine_types(Host:Port, TypesList) :-
+	format(atom(URL), 'http://~w:~w/types', [Host, Port]),
+	http_get(URL, JDoc, []),
+	atom_json_term(JDoc, JSon, []),
+	JSon = json(JList),
+	member(status=Status, JList),
+	(Status =:= 0 ->
+	member(types=TypesJSON, JList),
+	maplist(json_type, TypesJSON, TypesList)
+	;
+	member(status=Status, JList),
+	throw(status(Status))), !.
+
+json_type(json(List), Type) :-
+	member(class=Class, List),
+	member(name=Name, List),
+	member(value=TypeID, List),
+	findall(Attr, member(attribute=Attr, List), Attrs),
+	Type =.. [Class, TypeID, Name, Attrs].
+
+engine_save(Host:Port) :-
+	format(atom(URL), 'http://~w:~w/save', [Host, Port]),
+		http_get(URL, JDoc, []),
+	atom_json_term(JDoc, JSon, []),
+	JSon = json(JList),
+	member(status=Status, JList),
+	(Status =:= 0 ->
+	 true
+	;
+	 throw(status(Status))).
+
+engine_save(Host:Port, Name) :-
+	format(atom(URL), 'http://~w:~w/save?name=~w', [Host, Port, Name]),
+		http_get(URL, JDoc, []),
+	atom_json_term(JDoc, JSon, []),
+	JSon = json(JList),
+	member(status=Status, JList),
+	(Status =:= 0 ->
+	 true
+	;
+	 throw(status(Status))).
+
+engine_load(Host:Port) :-
+	format(atom(URL), 'http://~w:~w/restore', [Host, Port]),
+		http_get(URL, JDoc, []),
+	atom_json_term(JDoc, JSon, []),
+	JSon = json(JList),
+	member(status=Status, JList),
+	(Status =:= 0 ->
+	 true
+	;
+	 throw(status(Status))).
+
+engine_load(Host:Port, Name) :-
+	format(atom(URL), 'http://~w:~w/restore?name=~w', [Host, Port, Name]),
+		http_get(URL, JDoc, []),
+	atom_json_term(JDoc, JSon, []),
+	JSon = json(JList),
+	member(status=Status, JList),
+	(Status =:= 0 ->
+	 true
+	;
+	 throw(status(Status))).
---
+++ loader.pl
@@ -0,0 +1,222 @@
+% loader.pl - prolog loader interface
+
+% Copyright 2015, Simularity, Inc.
+
+:- module(loader, [engine_load/2]).
+:- use_module(engine_access).
+
+:- use_module(library(http/http_header)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/json)).
+
+% These are the predicates that get used by load_engine
+:- thread_local segment/3, segment_segs/1, object_map/3, type_map/2, action_map/2,
+	itemsize/2.
+
+% These are caching versions of the engine_access predicates for use in iterative
+% loads
+get_cached_type(Source, Name, Item) :-
+	(type_map(Name, Item) ->
+	 Item =..[_, Offset],
+	 type_offset(Offset)
+	;
+	 engine_get_type(Source, Name, Item),
+	 Item =.. [_, Offset], 
+	 type_offset(Offset),
+	 (integer(Name) ; assertz(type_map(Name, Item))), !).
+
+
+get_cached_object(Source, Name, Type, Object) :-
+	(object_map(Name, Type, Object) ->
+	 item_value(Object)
+	;
+	 engine_get_object(Source, Name, Type, Object),
+	 item_value(Object),
+	 (integer(Name) ; assertz(object_map(Name, Type, Object))), !).
+
+get_cached_action(Source, Name, Action) :-
+	(action_map(Name, Action) ->
+	 true
+	;
+	 engine_get_action(Source, Name, Action),
+	 (integer(Name) ; assertz(action_map(Name, Action))), !).
+
+% This is the engine hash function
+engine_hash(Atom, Hash) :-
+	atom_codes(Atom, String),
+	engine_hash(String, 5381, Hash).
+
+engine_hash([], Hash, Hash).
+engine_hash([V | T], Val, Hash) :-
+	Next is (((Val << 5) + Val) + V) mod 0x7fffffffffffffff,
+	engine_hash(T, Next, Hash).
+
+
+
+clear_preds :-
+	retractall(segment(_, _, _)),
+	retractall(segment_segs(_)),
+	retractall(object_map(_, _, _)),
+	retractall(type_map(_, _)),
+	retractall(action_map(_, _)).
+
+% connect sets thread_local predicates that guide the loading
+connect(Host:Port) :-
+	clear_preds,
+	format(atom(URL), 'http://~w:~w/segments', [Host, Port]),
+	% This one doesn't have keep alive
+	http_get(URL, JSON, []),
+	atom_json_term(JSON, JTerm, []),
+	JTerm = json(JSONList),
+	member(segments=JList, JSONList),
+
+	maplist(set_segment, JList),
+	length(JList, NSegs),
+	asserta(segment_segs(NSegs)), get_item_size(Host:Port), !.
+
+connect(_) :-
+	throw(error(connect_failed), _).
+
+
+
+get_item_size(_) :-
+	asserta(itemsize(0, 0xffffffff)).
+
+
+% set_segment sets the tl predicate segment/2 which allows access to the segments 
+set_segment(json(List)) :-
+	member(segment=Seg, List),
+	member(host=Host, List),
+	member(sport=Sport, List),
+	thread_create(segment_service(Host:Sport), TID, []),
+	assertz(segment(Seg, Host:Sport, TID)).
+
+
+segment_service(Host:Port) :-
+	segment_service(Host:Port, 0, []).
+
+segment_service(Host:Port, 10000, List) :-
+	flush(Host:Port, List),!,
+	segment_service(Host:Port, 0, []).
+
+segment_service(Host:Port, M, List) :-
+	thread_get_message(Message),
+	(Message = terminate(Force) ->
+	 (Force ; flush(Host:Port, List)) ,!
+	;
+	 Message = triple(_LHS, _Action, _RHS),
+	 NextList = [Message | List],
+	 N is M + 1,
+	 segment_service(Host:Port, N, NextList)).
+
+flush(Host:Port, List) :-
+	format(atom(URL), 'http://~w:~w/load_data', [Host, Port]),
+	write_to_codes(List, Codes),
+	http_post(URL, codes(Codes), Result, []),
+	atom_json_term(Result, JTerm, []),
+	JTerm = json(JSONList),
+	member(status=Status, JSONList),
+	Status = 0, !.
+
+flush(Source, _) :-
+	throw(error(load_error(Source), flush/2)).
+
+transform_subject(_Source, subject(Type, Item), subject(Type, Item)).
+transform_subject(Source, subject(TimeLine, ContextType, Context, Stamp, Bits),
+		  subject(TimeLine, Item)) :-
+
+	get_cached_object(Source, Context, object(ContextType), CtxID),
+	Mask is (1 << Bits) - 1,
+	(Stamp =:= Stamp /\ Mask ->
+	 true
+	;
+	 throw(error(stamp_overflow(Stamp, Bits), transform_subject/3))),
+	Item is (CtxID << Bits) \/ Stamp,
+	item_value(Item).
+	
+
+engine_load_(Host:Port, Closure) :-
+	catch(connect(Host:Port), Ex, (clear_preds, throw(Ex))),
+	get_item_size(Host:Port),
+	call(Closure, Subject, Action, Object),
+	transform_subject(Host:Port, Subject, XFormSubj), 
+	apply(Host:Port, XFormSubj, Action, Object),
+	thread_exceptions,
+	fail.
+
+engine_load_(_, _) :-
+	thread_exceptions, 
+	term_threads(false),
+	clear_preds.
+
+engine_load(Source, Closure) :-
+	catch(engine_load_(Source, Closure), X,
+	      (term_threads(true), clear_preds, throw(X))).
+
+term_threads(Force) :-
+	segment(_Seg, _Host, TID),
+	(catch(thread_property(TID, status(running)), _, fail) ->
+	 thread_send_message(TID, terminate(Force))
+	;
+	 true),
+	thread_join(TID, _),
+	fail.
+
+term_threads(_).
+
+
+thread_exceptions :-
+	segment(Seg, Host, TID),
+	thread_property(TID, status(exception(X))), !,
+	throw(error(segment_thread(Seg, Host, X))).
+
+thread_exceptions.
+
+
+apply(Host:Port, Subject, Action, Object) :-
+	Subject = subject(ST, SI),
+	Object = object(OT, OI),
+
+	get_cached_type(Host:Port, subject(ST), subject(STN)),
+	get_cached_type(Host:Port, object(OT), object(OTN)),
+	get_cached_object(Host:Port, SI, subject(STN), SIN),
+	get_cached_object(Host:Port, OI, object(OTN), OIN),
+	get_cached_action(Host:Port, Action, AN),
+
+% Convert the Items to strings to hash them into the segments
+	atom_number(SINA, SIN),
+	atom_number(OINA, OIN),
+% Hash Them
+	engine_hash(SINA, SINH),
+	engine_hash(OINA, OINH),
+% Convert to Segments
+	segment_segs(Segs),
+	SSEG is SINH mod Segs,
+	OSEG is OINH mod Segs,
+
+% Send it to the Segments
+	apply_seg(OSEG, subject(STN, SIN), AN, object(OTN, OIN)),
+	apply_seg(SSEG, object(OTN, OIN), AN, subject(STN, SIN)).
+
+apply_seg(Seg, LHS, A, RHS) :-
+	segment(Seg, _, TID),
+	thread_send_message(TID, triple(LHS, A, RHS)).
+
+
+type_offset(Value) :-
+	Value =< 0x7fff,
+	Value > 0, !.
+
+type_offset(Value) :-
+	type_error(sim_type, Value).
+
+item_value(Value) :-
+
+	itemsize(Min, Max),
+	Value >= Min,
+	Value =< Max, !.
+
+item_value(Value) :-
+	itemsize(Min, Max), 
+	type_error(Min-Max, Value).
+
